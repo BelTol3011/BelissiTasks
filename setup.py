9253ec81a17ca948877ca8477e56d1ea46bb7653
Bela Tollkien <37810842+BelTol3011@users.noreply.github.com>
2022-01-09 00:53:16 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup(
+        name='belissitasks',
+        version='1.0',
+        packages=['belissitasks'],
+        url='',
+        license='',
+        author='Belissimo',
+        author_email='bela.tollking@gmail.com',
+        description=''
+    )
